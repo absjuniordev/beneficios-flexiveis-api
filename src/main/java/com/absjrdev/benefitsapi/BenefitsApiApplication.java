7d15c54e74e986c2b1336157d64b43f01d8006da
Arnaldo Junior <abs.junnior@hotmail.com>
2025-11-17 10:58:59 -0300

    Initial commit

---
+++ src/main/java/com/absjrdev/benefitsapi/BenefitsApiApplication.java
@@ -0,0 +1,13 @@
+package com.absjrdev.benefitsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenefitsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/absjrdev/benefitsapi/BenefitsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absjrdev.benefitsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenefitsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
